fbfc6a3381588727eb7e908c69df92c6651adc40
Sadiq Hosainy <moh.sadiq22@gmail.com>
2024-04-23 15:37:14 +0100

    Final commit

--- Pages/PizzaList.cshtml.cs
+++ Pages/PizzaList.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
 
 namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
+{
+    private readonly PizzaService _service;
+    public IList<Pizza> PizzaList { get;set; } = default!;
+
+    public PizzaListModel(PizzaService service)
     {
-        public void OnGet()
-        {
-        }
+        _service = service;
     }
+
+    public void OnGet()
+    {
+        PizzaList = _service.GetPizzas();
+    }
+}
 }
